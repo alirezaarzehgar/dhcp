421fc671a4deb8bcc62483d08cc012fc85b6c11b
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-09-19 11:59:59 +0430

    init main test

---
+++ src/tests/main/include/main.h
@@ -0,0 +1,11 @@
+#if !defined(TESTS_MAIN_H)
+#define TESTS_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <CUnit/Basic.h>
+
+#endif // TESTS_MAIN_H
---
+++ src/tests/main/src/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author alireza arzehgar (alirezaarzehgar82@gmail.com)
+ * @brief endpoint for tests
+ * @version 0.1
+ * @date 2021-09-19
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "main.h"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
